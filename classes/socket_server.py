3e4ba220840149324e8c905d5a29e35843e8d39f
renantescaro <renantescaro@yahoo.com.br>
2023-04-10 15:58:24 -0300

    feat> refactoring socket

---
+++ classes/socket_server.py
@@ -0,0 +1,29 @@
+import socket
+
+class SocketServer:
+    def __init__(self) -> None:
+        self._host = ''
+        self._port = 5001
+        self._origin = (self._host, self._port)
+        self._tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection:socket.socket|None = None
+        self.client = None
+        self._listen()
+
+    def _listen(self):
+        self._tcp.bind(self._origin)
+        self._tcp.listen(1)
+
+    def _accept(self):
+        self.connection, self.client = self._tcp.accept()
+        print(f'conectado por {self.client}')
+
+    def execute(self):
+        if self.connection is not None:
+            return self.connection.recv(1024)
+
+        self._accept()
+        return None
+
+    def disconnect(self):
+
--- inicial.py
+++ inicial.py
@@ -1,33 +1,19 @@
-import socket
 import os
-
 from classes.rotina import Rotina
+from classes.socket_server import SocketServer
 # from classes.ultra_sonico import UltraSonico
-
-HOST = ''
-PORT = 5001
-ORIGEM = (HOST, PORT)
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-tcp.bind(ORIGEM)
-tcp.listen(1)
 
 # UltraSonico().start()
 
 os.system('git -C /home/pi/raspberry-python-robo-server/ pull origin master')
 
+socket_server = SocketServer()
 rotina = Rotina()
 
+
 while True:
-    conexao, cliente = tcp.accept()
-    print(f'conectado por {cliente}')
+    received_socket = socket_server.execute()
 
-    while True:
-        recebido_socket = conexao.recv(1024)
-        if not recebido_socket: break
+    if not received_socket: break
 
-        rotina.executar(recebido_socket)
-    
-    print(f'conexão finalizada com o cliente {cliente}')
-    conexao.close
+    rotina.executar(received_socket)
